b106fc74b666c1a966265237a2dfa43467f0d2f6
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-22 18:48:18 +0200

    Implemented CRUD views for Task and Tag. Added Complete and Undo functionality for tasks.

--- todo_list_app/models.py
+++ todo_list_app/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=40, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField(max_length=400, unique=True)
     datetime = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
     done = models.BooleanField(default=False)
-    tag = models.ManyToManyField(
+    tags = models.ManyToManyField(
         Tag,
         related_name="tasks",
     )
--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from todo_list_app.forms import TaskForm, TagForm
+from todo_list_app.models import Task, Tag
+
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "todo_list_app/home.html"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list_app:task-list")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list_app:task-list")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list_app:task-list")
+
+
+class TagListView(ListView):
+    model = Tag
+    context_object_name = "tags_list"
+    template_name = "todo_list_app/tags_list.html"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    form_class = TagForm
+    template_name = "todo_list_app/tags_form.html"
+    success_url = reverse_lazy("todo_list_app:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo_list_app:tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "todo_list_app/tags_confirm_delete.html"
+    success_url = reverse_lazy("todo_list_app:tag-list")
+
+
+class TaskCompleteView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.done = True
+        task.save()
+        return redirect("todo_list_app:task-list")
+
+
+class TaskUndoView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.done = False
+        task.save()
+        return redirect("todo_list_app:task-list")
